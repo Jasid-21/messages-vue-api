6344c6467cb943edfaddd5c21317070519ec822e
Jasid Martinez <jasidmartinez21@gmail.com>
2023-02-09 14:39:58 -0500

    Problem fixed: createPool implemented

--- connection/connection.js
+++ connection/connection.js
@@ -2,19 +2,11 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Database connection established!');
-    }
 });
 
 module.exports = connection;
