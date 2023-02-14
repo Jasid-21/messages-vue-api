4d353f820f4ff18edbff0ed521a6cec0f3f0ef7e
Jasid Martinez <jasidmartinez21@gmail.com>
2023-02-14 10:57:31 -0500

    Added sql sentences to create the databse

---
+++ README.md
@@ -0,0 +1,14 @@
+# app
+
+## Project setup
+```
+npm install
+npm install dotenv
+```
+
+## Create database
+```
+
+Execute the sentences included in database folder in order to create the database.
+This database will be empty. Register new users and test the app!
+```
--- connection/connection.js
+++ connection/connection.js
@@ -2,11 +2,20 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
 });
+
+connection.connect((err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+    console.log("Database connection made successfully!");
+})
 
 module.exports = connection;
